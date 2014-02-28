cc71332668973ea3ade15ebe75619e48c3686e15
Derp Herp <vlidaine@ubuntu.(none)>
2014-02-27 17:14:55 -0800

    working version without line removal

--- removestudent.sh
+++ removestudent.sh
@@ -1,12 +1,12 @@
 #!/bin/sh 
 # args: pennkey
-echo "are you sure you want to delete user $1? (Y/n)"
+echo -n "are you sure you want to delete user $1? (Y/n)"
 read bo
 echo $bo
-if [ "$bo" = "Y" ] then 
-	echo "you said yes, user $1 will now be deleted."
-elif [ "$bool" = "n" ] then
-	echo "you said no, script will now terminate."
+if [ x"$bo" = x"Y" ]; then 
+  echo "you said yes, user $1 will now be deleted."
+elif [ x"$bo" = x"n" ]; then
+  echo "you said no, script will now terminate."
 else
-	echo "Invalid input, script will now terminate."
+  echo "Invalid input, script will now terminate."
 fi
