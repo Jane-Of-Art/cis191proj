e3663d69b9df91a687512e4b8a023f9d6d962364
lianhanloh <loh.lianhan@gmail.com>
2014-03-02 01:11:02 -0500

    removestudent.sh works

--- addstudent.sh
+++ addstudent.sh
@@ -1,9 +1,9 @@
 #!/bin/sh 
 # args: pennkey, lastname, firstname. Assignments default to 0
 numcommas=$(head -n 1 studentlist.csv | fgrep -o , | wc -l)
-echo -n "$1, $2, $3" >> studentlist.csv
+printf "$1, $2, $3" >> studentlist.csv
 for i in $(seq 3 $numcommas)
 do
-	echo -n ", 0" >> studentlist.csv
+	printf ", 0" >> studentlist.csv
 done
 echo "" >> studentlist.csv
--- removestudent.sh
+++ removestudent.sh
@@ -6,12 +6,14 @@ lineNum=$(cut -d',' -f1 studentlist.csv | grep -wn $1 | cut -d : -f1)
 if [ -z $lineNum ]; then 
 	echo  "sorry, student is not listed in the file."
 else	
-	echo -n "are you sure you want to delete student $1? (Y/n) "
+	printf "are you sure you want to delete student $1? (Y/n) "
 	read bo
 	if [ x"$bo" = x"Y" ]; then 
 		echo "you said yes, student $1 will now be deleted."
-		#code to delete line here
-		sed ${lineNum}d studentlist.csv > studentlist.csv		
+		# delete line here
+		sed ${lineNum}d studentlist.csv > tmp.csv 
+		cat < tmp.csv > studentlist.csv
+		rm tmp.csv
 	elif [ x"$bo" = x"n" ]; then
 		echo "you said no, script will now terminate."
 	else
