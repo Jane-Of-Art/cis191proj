41ff3323b417c16b69258b389eb3b8a0abcd5dea
lianhanloh <loh.lianhan@gmail.com>
2014-03-03 23:03:09 -0500

    generateOneGrade.sh mostly done

--- generateOneGrade.sh
+++ generateOneGrade.sh
@@ -10,13 +10,31 @@ if [ -z $lineNum ]; then
 else 
 	printf "Please input name of assignment: "
 	read assignmentLabel
-	if [ -z "$(head -n 1 studentlist.csv | grep -w ${assignmentLabel})" ]; then
+	
+	IFS=$'\n' read -d '' -r -a lines < studentlist.csv
+	IFS=',' read -d '' -r -a line1 <<< "${lines[0]}"
+	IFS=',' read -d '' -r -a line2 <<< "${lines[1]}"
+	numvars=${#line1[@]}
+	colNum=0
+	for i in $(seq 3 $(($numvars-1)))
+	do
+		test=$(echo ${line1[$i]} | grep -w $assignmentLabel) 
+		if [[ $test = $assignmentLabel ]]; then
+			colNum=$(($i+1))	
+		fi
+	done
+	# if [ -z "$(head -n 1 studentlist.csv | grep -w ${assignmentLabel})" ]; then
+	if [[ $colNum -eq 0 ]]; then	
 		printf "Sorry, this assignment is not listed in the file.\n"		
 	else
 		touch ${pennkey}_${assignmentlabel}_grade
-		lastName=$(sed ${lineNum}p | cut -d ',' -f2)
-		firstName=$(sed ${lineNum}p | cut -d ',' -f3)
-		grade=$(sed ${lineNum}p | cut -d ',' -f3)
-		printf $lastName $firstName $grade
+		lastName=$(sed -n ${lineNum}p studentlist.csv | cut -d ',' -f2)
+		firstName=$(sed -n ${lineNum}p studentlist.csv | cut -d ',' -f3)
+		grade=$(sed -n ${lineNum}p studentlist.csv | cut -d ',' -f${colNum})
+		maxGrade=${line2[$(($colNum-1))]}
+		output="${firstName} ${lastName} (${pennkey})\n"
+		output2="${assignmentLabel}\nGrade: ${grade}/${maxGrade}\n"
+		printf "${firstName} ${lastName} (${pennkey})\n"
+		printf "${assignmentLabel}\nGrade: ${grade}/${maxGrade}\n"
 	fi
 fi
