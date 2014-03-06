5480c127e74ee8596dec9b7c35c578ad90a366fb
Lianhan Loh <lianhan@MOSH.seas.upenn.edu>
2014-03-05 20:40:07 -0500

    sendOneEmail.sh done

--- sendOneEmail.sh
+++ sendOneEmail.sh
@@ -15,24 +15,24 @@ else
 		printf "Sorry, this assignment is not listed in the file.\n"		
 	else
 		filename="${pennkey}_${assignmentLabel}_grade"
-		file=$(ls -l | grep -w $filename | grep ^'-')
-		dir=$(ls -l | grep $assignmentLabel | grep ^d)
-		if [[ -z $file && -z $dir ]]; then
+		if [[ ! -e $filename && ! -d $assignmentLabel ]]; then
 			printf "Grade file does not exist.\n"
 			printf "Please run \"./generateOneGrade.sh\" or \"./generateGradeDirectory.sh\"\n." 
-		elif [[ -n $file ]]; then
+		else
 			lineNum=$(cut -d',' -f1 studentlist.csv | grep -wn $pennkey | cut -d : -f1)
 			lastName=$(sed -n ${lineNum}p studentlist.csv | cut -d ',' -f2)
 			firstName=$(sed -n ${lineNum}p studentlist.csv | cut -d ',' -f3)
 			subject="${assignmentLabel} grade for ${firstName} ${lastName}" 
-			dos2unix $filename
-			contents="$(cat $filename) ^D"
-			# mail -s $($subject) "$(${pennkey}@seas.upenn.edu)" <<<  $($contents)
-			mail -s $($subject) "$(${pennkey}@seas.upenn.edu)" <  $filename
-			# echo $contents | sed s/' '/'\ '/g | mail -s $($subject) "$(${pennkey}@seas.upenn.edu)" 
-			mail -s "testfromscript" loh.lianhan@gmail.com < tux_exam1_grade
-			mail -s "testfromscript" lianhan@seas.upenn.edu < tux_exam1_grade
-			echo reached
+			if [[ -e $filename ]]; then
+				mail -s "$subject" "${pennkey}@seas.upenn.edu" <  $filename
+			elif [[ -d $assignmentLabel ]]; then
+				if [[ ! -e "${assignmentLabel}/${filename}" ]]; then
+					printf "Grade file does not exist in assignment directory.\n"
+					printf "Please run \"./generateGradeDirectory.sh\" again.\n"
+				else
+					mail -s "$subject" "${pennkey}@seas.upenn.edu" < "${assignmentLabel}/${filename}"
+				fi
+			fi
 		fi
 	fi
 fi
